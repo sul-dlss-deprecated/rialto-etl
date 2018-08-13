7dc4c7d64e75cdaba3977fa1bd306a3fe0a0a8b2
Justin Coyne <jcoyne@justincoyne.com>
2018-08-13 13:43:08 -0500

    Paginate results

--- lib/rialto/etl/extractors/stanford_researchers.rb
+++ lib/rialto/etl/extractors/stanford_researchers.rb
@@ -3,25 +3,46 @@
 module Rialto
   module Etl
     module Extractors
-      require 'byebug'
       # Stanford Profiles API
       class StanfordResearchers
-        def initialize(client: StanfordClient.new)
+        def initialize(client: StanfordClient.new, per_page: 100, start_page: 1)
           @client = client
+          @per_page = per_page # 100 seems to be the max the API allows
+          @page = start_page
+          @more = true
         end
 
         # Hit an API endpoint and return the results
         def each(&_block)
           return to_enum(:each) unless block_given?
-          per_page = 100 # 100 seems to be the max the API allows
-          yield client.get("/profiles/v1?p=1&ps=#{per_page}")
+          while more
+            # Retrieve a page of results
+            result = fetch_results_for_page(page)
+            # Yield the block for each result on the page
+            result['values'].each do |val|
+              yield val
+            end
+          end
         rescue StandardError => exception
           puts "Error: #{exception.message}"
         end
 
         private
 
-        attr_reader :client
+        attr_reader :client, :per_page
+        attr_accessor :more, :page
+
+        def fetch_results_for_page(page)
+          result = client.get(path(page))
+          json = JSON.parse(result)
+          self.more = !json.fetch('lastPage')
+          self.page = page + 1 if more
+          json
+        end
+
+        def path(page)
+          "/profiles/v1?p=#{page}&ps=#{per_page}"
+        end
       end
     end
   end
--- spec/extractors/stanford_researchers_spec.rb
+++ spec/extractors/stanford_researchers_spec.rb
@@ -14,14 +14,10 @@ RSpec.describe Rialto::Etl::Extractors::StanfordResearchers do
     before do
       stub_request(:get, /authz.stanford.edu/)
         .to_return(status: 200, body: authz_json, headers: {})
-      stub_request(:get, /api.stanford.edu/)
-        .to_return(status: 200, body: 'whatever', headers: {})
-    end
-
-    it { is_expected.to respond_to(:each) }
-
-    it 'does not raise NotImplementedError' do
-      expect { extractor.each }.not_to raise_error
+      stub_request(:get, 'https://api.stanford.edu/profiles/v1?p=1&ps=100')
+        .to_return(status: 200, body: '{"lastPage": false, "values":["one"]}', headers: {})
+      stub_request(:get, 'https://api.stanford.edu/profiles/v1?p=2&ps=100')
+        .to_return(status: 200, body: '{"lastPage": true, "values":["two", "three"]}', headers: {})
     end
 
     context 'when client raises an exception' do
@@ -35,6 +31,16 @@ RSpec.describe Rialto::Etl::Extractors::StanfordResearchers do
         allow(STDOUT).to receive(:puts)
         extractor.each { true }
         expect(STDOUT).to have_received(:puts).with("Error: #{error_message}")
+      end
+    end
+
+    context 'when there is more than one page of results' do
+      it 'calls the block on each result' do
+        results = []
+        extractor.each do |records|
+          results << records
+        end
+        expect(results).to eq %w[one two three]
       end
     end
   end
