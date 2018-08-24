7057a18eb2be89cb05e52cdae371a77128e26407
Justin Coyne <jcoyne@justincoyne.com>
2018-08-24 16:04:44 -0500

    Add generic types to the organization json.
    Fixes #65

--- spec/transform/organization_list_to_jsonld_spec.rb
+++ spec/transform/organization_list_to_jsonld_spec.rb
@@ -8,7 +8,11 @@ RSpec.describe 'Transforming organization list to jsonld' do
   let(:config_file_path) { 'lib/rialto/etl/configs/organizations_to_jsonld.rb' }
   let(:transformer) { Rialto::Etl::Transformer.new(input_stream: input_stream, config_file_path: config_file_path) }
 
+  let(:division) { 'http://vivoweb.org/ontology/core#Division' }
+  let(:agent) { 'http://xmlns.com/foaf/0.1/Agent' }
+  let(:organization) { 'http://xmlns.com/foaf/0.1/Organization' }
+
   it 'outputs transformer results to stdout' do
-    expect { transformer.transform }.to output.to_stdout
+    expect { transformer.transform }.to output(/"@type":\["#{division}","#{agent}","#{organization}"\]/).to_stdout
   end
 end
