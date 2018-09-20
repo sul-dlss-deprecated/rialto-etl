f6a8d1cab42c33ea40261452ee982a87a428c9d0
Justin Coyne <jcoyne@justincoyne.com>
2018-09-20 11:49:48 -0500

    Extract the EntityResolver client from WoS config
    Fixes #114

---
+++ spec/service_client/entity_resolver_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+RSpec.describe Rialto::Etl::ServiceClient::EntityResolver do
+  describe '.resolve' do
+    subject(:resolve) do
+      described_class.resolve('person', 'orcid_id' => '0000-0002-2328-2018', 'full_name' => 'Wilson, Jennifer L.')
+    end
+
+    context 'when the record is found' do
+      before do
+        stub_request(:get, 'http://127.0.0.1:3001/person?full_name=Wilson,%20Jennifer%20L.&orcid_id=0000-0002-2328-2018')
+          .to_return(status: 200, body: 'http://sul.stanford.edu/rialto/agents/people/123')
+      end
+      it 'gets the URI' do
+        expect(resolve).to eq 'http://sul.stanford.edu/rialto/agents/people/123'
+      end
+    end
+
+    context 'when the record is not found' do
+      before do
+        stub_request(:get, 'http://127.0.0.1:3001/person?full_name=Wilson,%20Jennifer%20L.&orcid_id=0000-0002-2328-2018')
+          .to_return(status: 404, body: '')
+      end
+      it 'gets the URI' do
+        expect(resolve).to be_nil
+      end
+    end
+  end
+end
