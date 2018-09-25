afad3dc2d5c2533399452ad095d8c8dbbfadc4f9
Justin Littman <justinlittman@stanford.edu>
2018-09-25 15:33:03 -0400

    closes #151. Adds logging to Sparql statement reader.

--- lib/rialto/etl/readers/sparql_statement_reader.rb
+++ lib/rialto/etl/readers/sparql_statement_reader.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require 'yell'
+require 'active_support'
+require 'active_support/core_ext/numeric/conversions'
+
 module Rialto
   module Etl
     module Readers
@@ -11,13 +15,22 @@ module Rialto
         include Enumerable
 
         attr_reader :settings, :input_stream
+        attr_accessor :insert_count
 
         def initialize(input_stream, settings)
           @settings = settings
           @input_stream = input_stream
+          @insert_count = 0
+        end
+
+        # Get the logger from the settings, or default to an effectively null logger
+        def logger
+          settings['logger'] ||= Yell.new(STDERR, level: 'gt.fatal') # null logger
         end
 
         # rubocop:disable Metrics/MethodLength
+        # rubocop:disable Metrics/AbcSize
+        # rubocop:disable Metrics/CyclomaticComplexity
         def each
           return enum_for(:each) unless block_given?
 
@@ -28,6 +41,8 @@ module Rialto
             statement << line
             next unless statement.end_with?(";\n")
             statements << statement
+            logger.info("Read #{insert_count.to_s(:delimited)} INSERT statements.") if statement.start_with?('INSERT') &&
+                                                                                       ((@insert_count += 1) % 1000).zero?
             if statement.start_with?('INSERT') || settings['sparql_statement_reader.by_statement']
               yield statements
               statements = +''
@@ -36,6 +51,8 @@ module Rialto
           end
         end
         # rubocop:enable Metrics/MethodLength
+        # rubocop:enable Metrics/AbcSize
+        # rubocop:enable Metrics/CyclomaticComplexity
       end
     end
   end
