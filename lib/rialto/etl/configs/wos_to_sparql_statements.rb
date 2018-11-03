d6465d3b020ac5d19ec51fdef7887fe9c12aadad
Michael J. Giarlo <mjgiarlo@stanford.edu>
2018-11-03 10:43:43 -0700

    Use preferred DOI resolver
    See https://github.com/sul-dlss/rialto-webapp/pull/153

--- lib/rialto/etl/configs/wos_to_sparql_statements.rb
+++ lib/rialto/etl/configs/wos_to_sparql_statements.rb
@@ -120,7 +120,7 @@ to_field "!#{RDF::Vocab::BIBO.doi}", literal(true), single: true
 to_field RDF::Vocab::BIBO.doi.to_s, lambda { |json, accumulator|
   doi = JsonPath.on(json, '$.dynamic_data.cluster_related.identifiers.identifier[?(@.type=="doi")].value').first ||
         JsonPath.on(json, '$.dynamic_data.cluster_related.identifiers.identifier[?(@.type=="xref_doi")].value').first
-  accumulator << RDF::URI("http://dx.doi.org/#{doi}") if doi
+  accumulator << RDF::URI("https://doi.org/#{doi}") if doi
 }, single: true
 
 to_field "!#{VIVO.relatedBy}", literal(true), single: true
