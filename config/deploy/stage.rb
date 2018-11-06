3d6c28bd4557f570a2a6cf88567e26b92a62c89e
Justin Coyne <jcoyne@justincoyne.com>
2018-11-06 09:43:33 -0600

    Add Capistrano for deployment
    Fixes #228

---
+++ config/deploy/stage.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+server 'rialto-vitro-dev.stanford.edu', user: 'vitro', roles: %w[app]
+Capistrano::OneTimeKey.generate_one_time_key!
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult the Net::SSH documentation.
+# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
+#
+# Global options
+# --------------
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+#
+# The server-based syntax can be used to override options:
+# ------------------------------------
+# server "example.com",
+#   user: "user_name",
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: "user_name", # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: "please use keys"
+#   }
