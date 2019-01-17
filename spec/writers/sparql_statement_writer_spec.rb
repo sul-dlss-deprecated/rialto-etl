2e56a15bb641314429ec5b184990a40b46ab74e7
Justin Littman <justinlittman@stanford.edu>
2019-01-17 17:18:21 -0500

    Fixes problem where array value was producing multiple SPARQL insert statements.

--- spec/writers/sparql_statement_writer_spec.rb
+++ spec/writers/sparql_statement_writer_spec.rb
@@ -67,54 +67,71 @@ RSpec.describe Rialto::Etl::Writers::SparqlStatementWriter do
   end
 
   describe '#serialize' do
-    let(:first_hash) do
-      {
-        '@id' => Rialto::Etl::Vocabs::RIALTO_PEOPLE['1234'].to_s,
-        '@graph' => Rialto::Etl::NamedGraphs::STANFORD_PEOPLE_GRAPH.to_s,
-        '@type' => [RDF::Vocab::FOAF.Person, Rialto::Etl::Vocabs::Stanford.Staff],
-        Rialto::Etl::Vocabs::VIVO.overview.to_s => 'Justin Littman is a software developer and librarian.',
-        RDF::Vocab::VCARD.hasEmail.to_s => 'jlittypo@example.org',
-        '#advisee' => {
-          '@id' => Rialto::Etl::Vocabs::RIALTO_PEOPLE['188882'].to_s,
+    describe 'when serializing a hash' do
+      let(:first_hash) do
+        {
+          '@id' => Rialto::Etl::Vocabs::RIALTO_PEOPLE['1234'].to_s,
           '@graph' => Rialto::Etl::NamedGraphs::STANFORD_PEOPLE_GRAPH.to_s,
-          '@type' => [RDF::Vocab::FOAF.Person],
-          RDF::Vocab::VCARD.hasName.to_s => Rialto::Etl::Vocabs::RIALTO_CONTEXT_NAMES['188882']
+          '@type' => [RDF::Vocab::FOAF.Person, Rialto::Etl::Vocabs::Stanford.Staff],
+          Rialto::Etl::Vocabs::VIVO.overview.to_s => 'Justin Littman is a software developer and librarian.',
+          RDF::Vocab::VCARD.hasEmail.to_s => 'jlittypo@example.org',
+          '#advisee' => {
+            '@id' => Rialto::Etl::Vocabs::RIALTO_PEOPLE['188882'].to_s,
+            '@graph' => Rialto::Etl::NamedGraphs::STANFORD_PEOPLE_GRAPH.to_s,
+            '@type' => [RDF::Vocab::FOAF.Person],
+            RDF::Vocab::VCARD.hasName.to_s => Rialto::Etl::Vocabs::RIALTO_CONTEXT_NAMES['188882']
+          }
         }
-      }
+      end
+
+      let(:second_hash) do
+        {
+          '@id' => Rialto::Etl::Vocabs::RIALTO_PEOPLE['1234'].to_s,
+          '@graph' => Rialto::Etl::NamedGraphs::STANFORD_PEOPLE_GRAPH.to_s,
+          '@type' => [RDF::Vocab::FOAF.Person, Rialto::Etl::Vocabs::Stanford.Staff],
+          "!#{RDF::Vocab::VCARD.hasEmail}" => true,
+          RDF::Vocab::VCARD.hasEmail.to_s => 'jlit@example.org'
+        }
+      end
+
+      let(:first_statements) { writer.serialize_hash(first_hash) }
+
+      let(:second_statements) { writer.serialize_hash(second_hash) }
+
+      before do
+        graph << [Rialto::Etl::Vocabs::RIALTO_PEOPLE['1234'], RDF.type, RDF::Vocab::FOAF.Person]
+        graph << [Rialto::Etl::Vocabs::RIALTO_PEOPLE['1234'], RDF.type, Rialto::Etl::Vocabs::Stanford.Staff]
+        graph << [Rialto::Etl::Vocabs::RIALTO_PEOPLE['1234'],
+                  Rialto::Etl::Vocabs::VIVO.overview,
+                  'Justin Littman is a software developer and librarian.']
+        graph << [Rialto::Etl::Vocabs::RIALTO_PEOPLE['1234'],
+                  RDF::Vocab::VCARD.hasEmail,
+                  'jlit@example.org']
+        graph << [Rialto::Etl::Vocabs::RIALTO_PEOPLE['188882'], RDF.type, RDF::Vocab::FOAF.Person]
+        graph << [Rialto::Etl::Vocabs::RIALTO_PEOPLE['188882'],
+                  RDF::Vocab::VCARD.hasName,
+                  Rialto::Etl::Vocabs::RIALTO_CONTEXT_NAMES['188882']]
+
+        execute_sparql!(first_statements)
+        execute_sparql!(second_statements)
+      end
+
+      it { is_expected.to have_same_triples(graph) }
     end
 
-    let(:second_hash) do
-      {
-        '@id' => Rialto::Etl::Vocabs::RIALTO_PEOPLE['1234'].to_s,
-        '@graph' => Rialto::Etl::NamedGraphs::STANFORD_PEOPLE_GRAPH.to_s,
-        '@type' => [RDF::Vocab::FOAF.Person, Rialto::Etl::Vocabs::Stanford.Staff],
-        "!#{RDF::Vocab::VCARD.hasEmail}" => true,
-        RDF::Vocab::VCARD.hasEmail.to_s => 'jlit@example.org'
-      }
+    describe 'when serializing a hash with an array' do
+      let(:hash) do
+        {
+          '@id' => Rialto::Etl::Vocabs::RIALTO_PEOPLE['1234'].to_s,
+          RDF::Vocab::SKOS.altLabel.to_s => ['Justin Littman', 'Littman, Justin']
+        }
+      end
+
+      let(:statements) { writer.serialize_hash(hash).flatten }
+
+      it 'produces a single insert for the array' do
+        expect(statements.length).to be(1)
+      end
     end
-
-    let(:first_statements) { writer.serialize_hash(first_hash) }
-
-    let(:second_statements) { writer.serialize_hash(second_hash) }
-
-    before do
-      graph << [Rialto::Etl::Vocabs::RIALTO_PEOPLE['1234'], RDF.type, RDF::Vocab::FOAF.Person]
-      graph << [Rialto::Etl::Vocabs::RIALTO_PEOPLE['1234'], RDF.type, Rialto::Etl::Vocabs::Stanford.Staff]
-      graph << [Rialto::Etl::Vocabs::RIALTO_PEOPLE['1234'],
-                Rialto::Etl::Vocabs::VIVO.overview,
-                'Justin Littman is a software developer and librarian.']
-      graph << [Rialto::Etl::Vocabs::RIALTO_PEOPLE['1234'],
-                RDF::Vocab::VCARD.hasEmail,
-                'jlit@example.org']
-      graph << [Rialto::Etl::Vocabs::RIALTO_PEOPLE['188882'], RDF.type, RDF::Vocab::FOAF.Person]
-      graph << [Rialto::Etl::Vocabs::RIALTO_PEOPLE['188882'],
-                RDF::Vocab::VCARD.hasName,
-                Rialto::Etl::Vocabs::RIALTO_CONTEXT_NAMES['188882']]
-
-      execute_sparql!(first_statements)
-      execute_sparql!(second_statements)
-    end
-
-    it { is_expected.to have_same_triples(graph) }
   end
 end
